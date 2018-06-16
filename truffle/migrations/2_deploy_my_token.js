a56ea4b8439b2f331bce23507b231705deb040c2
Sho Iizuka <sho.i518@gmail.com>
2018-06-16 18:31:45 +0900

    Modify migration script

--- truffle/migrations/2_deploy_my_token.js
+++ truffle/migrations/2_deploy_my_token.js
@@ -1,5 +1,13 @@
 const MyToken = artifacts.require('./MyToken.sol')
+const fs = require('mz/fs');
 
 module.exports = (deployer) => {
-  deployer.deploy(MyToken, "MyToken", "MTKN")
+  deployer
+    .deploy(MyToken, "MyToken", "MTKN")
+    .then(() =>
+      Promise.all([
+        fs.writeFile('../contracts/address.txt', MyToken.address),
+        fs.writeFile('../contracts/abi.json', JSON.stringify(MyToken.toJSON().abi)),
+      ])
+    );
 }
